f3ce6af10f45bc75cb872aadad62d950c5fb4fc7
Vadym Hevlich <vege1wgw@gmail.com>
2018-01-27 22:05:30 +0200

    project setup

--- dds_project/configs/settings/base.py
+++ dds_project/configs/settings/base.py
@@ -1,8 +1,26 @@
 import os
+from django.core.exceptions import ImproperlyConfigured
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+def get_env_variable(var_name):
+    """Get the environment variable or return exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
+
+# Build paths inside the project like this: root('some_path')
+def root(*dirs):
+    base_dir = os.path.join(os.path.dirname(__file__), '..', '..')
+    return os.path.abspath(os.path.join(base_dir, *dirs))
+
+
+BASE_DIR = root()
+
+
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
@@ -33,7 +51,7 @@ ROOT_URLCONF = 'dds_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [root('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,3 +101,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
+STATIC_ROOT = root('static')
+STATICFILES_DIRS = [root('staticfiles')]
--- dds_project/configs/settings/local.py
+++ dds_project/configs/settings/local.py
@@ -6,7 +6,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('PG_NAME'),
+        'USER': get_env_variable('PG_USER'),
+        'PASSWORD': get_env_variable('PG_PASSWORD'),
+        'HOST': os.getenv('PG_HOST', '127.0.0.1'),
+        'PORT': os.getenv('PG_PORT', '5432')
     }
 }
