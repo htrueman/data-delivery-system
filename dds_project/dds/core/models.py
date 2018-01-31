f6668acfbfd1f2d45c8e65b5f49eb122b5ecc664
Vadym Hevlich <vege1wgw@gmail.com>
2018-01-31 21:58:02 +0200

    system user model manager provided

--- dds_project/dds/core/models.py
+++ dds_project/dds/core/models.py
@@ -1,17 +1,46 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+
+
+class SystemUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        if not email:
+            raise ValueError('Email is required.')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class SystemUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=40, unique=True)
     email = models.EmailField(max_length=50)
-    git_repository = models.ForeignKey('GitRepository', null=True, on_delete=models.CASCADE)
 
     USERNAME_FIELD = 'username'
+
+    objects = SystemUserManager()
 
 
 class GitRepository(models.Model):
     username = models.CharField(max_length=40)
     password = models.CharField(max_length=128)
     deep_link = models.CharField(max_length=150)
+    user = models.ForeignKey('SystemUser', on_delete=models.CASCADE)
